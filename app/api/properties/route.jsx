efb6283761e9e8b2bd7996a6545f6387031361ee
Elvan Hidayat <elvanhd@gmail.com>
2024-02-21 10:29:01 +0700

    Integrate MongoDB with API routes and models
    - Implement MongoDB connection utility in config/database.js
    - Create Property and User models with Mongoose for data management
    - Add API route for fetching properties with MongoDB integration
    - Adjust InfoBoxes component text for clarity in property listing intent
    - Include MongoDB and Mongoose in dependencies for database interaction
    - Setup environment for database connection string and other configurations
    - Ensure models support relationships, such as property ownership and user bookmarks
    - Utilize schema validation and default values for model integrity and ease of use

---
+++ app/api/properties/route.jsx
@@ -0,0 +1,18 @@
+import connectDB from '@/config/database';
+import Property from '@/models/Property';
+
+// GET /api/properties
+export const GET = async (request) => {
+  try {
+    await connectDB();
+
+    const properties = await Property.find({});
+
+    return new Response(JSON.stringify(properties), {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response('Something Went Wrong', { status: 500 });
+  }
+};
--- components/InfoBoxes.jsx
+++ components/InfoBoxes.jsx
@@ -25,7 +25,7 @@ const InfoBoxes = () => {
               backgroundColor: 'bg-blue-500',
             }}
           >
-            List your properties and reach potential tenants. Rent as an Airbnb or long term.
+            List your properties and reach potential tenants. Rent short or long term.
           </InfoBox>
         </div>
       </div>
