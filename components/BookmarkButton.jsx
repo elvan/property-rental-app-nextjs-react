dc187745581e761d1322361fd5ae110b8106701b
Elvan Hidayat <elvanhd@gmail.com>
2024-02-22 12:39:30 +0700

    Add bookmark functionality for properties
    - Implement API route for adding and removing bookmarks
    - Modify BookmarkButton component to toggle bookmark state
    - Use getSessionUser utility to validate user session
    - Enable dynamic bookmarking with visual feedback via toast notifications
    - Ensure bookmark changes persist in the User model
    - Adapt BookmarkButton to handle click events and update state accordingly
    - Include error handling for unauthorized actions or failures
    - Utilize react-icons for a consistent and visually appealing UI

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -1,8 +1,49 @@
-import { FaBookmark } from 'react-icons/fa';
+'use client';
 
-const BookmarkButton = () => {
+import { useSession } from 'next-auth/react';
+import { useState } from 'react';
+import { FaBookmark } from 'react-icons/fa';
+import { toast } from 'react-toastify';
+
+const BookmarkButton = ({ property }) => {
+  const { data: session } = useSession();
+  const userId = session?.user?.id;
+
+  const [isBookmarked, setIsBookmarked] = useState(false);
+
+  const handleClick = async () => {
+    if (!userId) {
+      toast.error('You need to sign in to bookmark a property');
+      return;
+    }
+
+    try {
+      const res = await fetch('/api/bookmarks', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          propertyId: property._id,
+        }),
+      });
+
+      if (res.status === 200) {
+        const data = await res.json();
+        toast.success(data.message);
+        setIsBookmarked(data.isBookmarked);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error('Something went wrong');
+    }
+  };
+
   return (
-    <button className='bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center'>
+    <button
+      onClick={handleClick}
+      className='bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center'
+    >
       <FaBookmark className='mr-2' /> Bookmark Property
     </button>
   );
