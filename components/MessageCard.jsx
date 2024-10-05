8f41bde8a55cdb87c8ed7e43b89ffc216a0159c3
Elvan Hidayat <elvanhd@gmail.com>
2024-10-05 21:24:25 +0700

    Implement messaging system with inbox and message functionality
    - Create `Message` model to store and manage user messages
    - Implement `addMessage`, `deleteMessage`, and `markMessageAsRead` actions
    - Add `getUnreadMessageCount` action to track unread messages
    - Create `MessagePage` to display user's messages (read and unread)
    - Add `MessageCard` component for displaying individual messages
    - Integrate message submission form in `PropertyContactForm`
    - Add `UnreadMessageCount` to Navbar for real-time message notifications
    - Create `GlobalContext` to manage global state for unread message count

---
+++ components/MessageCard.jsx
@@ -0,0 +1,82 @@
+'use client';
+
+import deleteMessage from '@/app/actions/deleteMessage';
+import markMessageAsRead from '@/app/actions/markMessageAsRead';
+import { useGlobalContext } from '@/context/GlobalContext';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+
+const MessageCard = ({ message }) => {
+  const [isRead, setIsRead] = useState(message.read);
+  const [isDeleted, setIsDeleted] = useState(false);
+
+  const { setUnreadCount } = useGlobalContext();
+
+  const handleReadClick = async () => {
+    const read = await markMessageAsRead(message._id);
+    setIsRead(read);
+    setUnreadCount((prevCount) => (read ? prevCount - 1 : prevCount + 1));
+    toast.success(`Marked as ${read ? 'read' : 'new'}`);
+  };
+
+  const handleDeleteClick = async () => {
+    await deleteMessage(message._id);
+    setIsDeleted(true);
+    setUnreadCount((prevCount) => (isRead ? prevCount : prevCount - 1));
+    toast.success('Message Deleted');
+  };
+
+  if (isDeleted) {
+    return <p>Deleted message</p>;
+  }
+
+  return (
+    <div className='relative bg-white p-4 rounded-md shadow-md border border-gray-200'>
+      {!isRead && (
+        <div className='absolute top-2 right-2 bg-yellow-500 text-white px-2 py-1 rounded-md'>
+          New
+        </div>
+      )}
+      <h2 className='text-xl mb-4'>
+        <span className='font-bold'>Property Inquiry:</span>{' '}
+        {message.property.name}
+      </h2>
+      <p className='text-gray-700'>{message.body}</p>
+
+      <ul className='mt-4'>
+        <li>
+          <strong>Reply Email:</strong>{' '}
+          <a href={`mailto:${message.email}`} className='text-blue-500'>
+            {message.email}
+          </a>
+        </li>
+        <li>
+          <strong>Reply Phone:</strong>{' '}
+          <a href={`tel:${message.phone}`} className='text-blue-500'>
+            {message.phone}
+          </a>
+        </li>
+        <li>
+          <strong>Received:</strong>{' '}
+          {new Date(message.createdAt).toLocaleString()}
+        </li>
+      </ul>
+      <button
+        onClick={handleReadClick}
+        className={`mt-4 mr-3 ${
+          isRead ? 'bg-gray-300' : 'bg-blue-500 text-white'
+        } py-1 px-3 rounded-md`}
+      >
+        {isRead ? 'Mark As New' : 'Mark As Read'}
+      </button>
+      <button
+        onClick={handleDeleteClick}
+        className='mt-4 bg-red-500 text-white py-1 px-3 rounded-md'
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export default MessageCard;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,6 +8,7 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { FaGoogle } from 'react-icons/fa';
+import UnreadMessageCount from './UnreadMessageCount';
 
 const Navbar = () => {
   const { data: session } = useSession();
@@ -152,10 +153,7 @@ const Navbar = () => {
                     />
                   </svg>
                 </button>
-                <span className='absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full'>
-                  2
-                  {/* <!-- Replace with the actual number of notifications --> */}
-                </span>
+                <UnreadMessageCount />
               </Link>
               {/* <!-- Profile dropdown button --> */}
               <div className='relative ml-3'>
